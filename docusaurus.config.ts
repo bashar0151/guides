8897792181b7ca225c853abf33c7dae6e6089b15
bashar0151 <notifications@scouse.me>
2026-04-05 15:39:25 +0000

    update github repor url

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -40,7 +40,7 @@ const config: Config = {
       {
         docs: {
           sidebarPath: './sidebars.ts',
-          editUrl: 'https://github.com/bashar0151/guides/blob/main/docs/',
+          editUrl: 'https://github.com/bashar0151/guides/tree/1d90b5c1b25981595a4a4cc432e88c709913371a/docs',
           routeBasePath: '/',
           showLastUpdateAuthor: true, 
           showLastUpdateTime: true,
